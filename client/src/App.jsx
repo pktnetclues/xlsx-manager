cc067abdb7482302514474898781fc3c9de52c9c
pankaj <pthakur.netclues@gmail.com>
2024-04-19 20:06:06 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Toaster } from "sonner";
 
 import "./App.css";
-import UploadXlsx from "./components/UploadXlsx";
-import ShowData from "./components/ShowData";
 import Navbar from "./components/Dashboard/Navbar";
 import PostCard from "./components/Dashboard/Card";
 
@@ -12,8 +10,6 @@ function App() {
     <Router>
       <Toaster position="top-center" />
       <Routes>
-        <Route path="/" element={<UploadXlsx />} />
-        <Route path="/show" element={<ShowData />} />
         <Route path="/card" element={<PostCard />} />
       </Routes>
       <Navbar />
